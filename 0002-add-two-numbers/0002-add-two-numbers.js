6891f21f0cdf55e3b94239f3548cd917573806a6
abdull_k1 <145838102+DarknesKon@users.noreply.github.com>
2024-06-09 16:20:32 +0600

    Time: 98 ms (88.82%), Space: 54.5 MB (95.78%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -1,24 +1,18 @@
 function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+    this.val = (val===undefined? 0 : val);
+    this.next = (next===undefined? null : next);
 }
 
 var addTwoNumbers = function(l1, l2) {
-    let head = new ListNode(0);
-    let node = head;
-    let carry = 0;
+    let head = new ListNode(0), node = head, carry = 0;
 
     while (l1 || l2) {
-        let l1Val = l1 ? l1.val : 0;
-        let l2Val = l2 ? l2.val : 0;
-        let sum = l1Val + l2Val + carry;
-
-        carry = sum >= 10 ? 1 : 0;
+        let sum = (l1? l1.val : 0) + (l2? l2.val : 0) + carry;
+        carry = sum >= 10? 1 : 0;
         node.next = new ListNode(sum % 10);
         node = node.next;
-
-        if (l1) l1 = l1.next;
-        if (l2) l2 = l2.next;
+        l1 = l1? l1.next : null;
+        l2 = l2? l2.next : null;
     }
 
     if (carry > 0) {
