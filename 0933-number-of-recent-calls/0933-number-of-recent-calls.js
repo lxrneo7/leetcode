ae48ebc2275cbf05566d892250962d3ca3a243c8
abdull_k1 <145838102+DarknesKon@users.noreply.github.com>
2024-06-23 15:57:47 +0600

    Time: 170 ms (92.18%), Space: 60.7 MB (88.27%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.js
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.js
@@ -1,21 +1,12 @@
-var RecentCounter = function () {
-    this.q = [];
-};
-
-/**
- * @param {number} t
- * @return {number}
- */
-RecentCounter.prototype.ping = function (t) {
-    this.q.push(t);
-    while (this.q[0] < t - 3000) {
-        this.q.shift();
+class RecentCounter {
+    constructor() {
+        this.requests = [];
     }
-    return this.q.length;
-};
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * var obj = new RecentCounter()
- * var param_1 = obj.ping(t)
- */
+    ping(t) {
+        this.requests.push(t);
+        while (this.requests[0] < t - 3000) {
+            this.requests.shift();
+        }
+        return this.requests.length;
+    }
+}
