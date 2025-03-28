8f247a3221f0b5c7bb6e2d9d0d92d86b6201518e
Abdurahim <145838102+lxrneo7@users.noreply.github.com>
2025-03-28 10:11:00 +0600

    Time: 58 ms (9.58%), Space: 58.4 MB (11.79%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -1,15 +1,16 @@
-//
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var removeDuplicates = function(nums) {
-  var len = nums.length;
-  var last = NaN;
-  var count = 0;
-  for (var i = 0; i < len; i++) {
-    if (nums[i] !== last) {
-      nums[count] = nums[i];
-      last = nums[i];
-      count++;
+    let numsLength = nums.length;
+    for (let i = 0; i < numsLength; i++) {
+        if (i >= nums.length) {
+            break;
+        }
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
     }
-  }
-  return count;
 };
-//
