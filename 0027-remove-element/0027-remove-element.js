b3020022a4d565a3fd6744e34ba56b2ad0c1b2ee
Abdurahim <145838102+lxrneo7@users.noreply.github.com>
2025-03-28 10:13:19 +0600

    Time: 0 ms (100%), Space: 53.4 MB (46.15%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -3,13 +3,15 @@
  * @param {number} val
  * @return {number}
  */
- var removeElement = function(nums, val) {
-    let i = 0;
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] !== val) {
-            nums[i] = nums[j];
-            i++;
+var removeElement = function(nums, val) {
+     let i = 0;
+      while (i < nums.length) {
+        if (nums.includes(val)) {
+          nums.splice(nums.indexOf(val), 1);
+        } else {
+          i++;
         }
-    }
-    return i;
+      }
+
+      return nums.length;
 };
